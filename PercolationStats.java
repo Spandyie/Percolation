33b6ce5b1f44c2026670e3f4013e58fdeff9a736
Spandan Mishra <smishra28@outlook.com>
2018-01-18 16:46:45 -0800

    Update PercolationStats.java

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
